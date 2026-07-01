25852e9b5317332c11d142a3f52adbd89f96c259
al4624 <al4624@ic.ac.uk>
2026-07-01 19:22:21 +0100

    Feat: remove top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import type { ConfiguratorOptions } from '@/shopify/integration';
 import { useStore } from '@/store/store';
-import { Header } from './components/Header';
 import { Studio } from './components/Studio';
 import { Sidebar } from './components/Sidebar';
 import { ErrorToast } from './components/ErrorToast';
@@ -22,7 +21,6 @@ export function App({ options = {} }: AppProps) {
   return (
     <div className="bcfg relative flex h-full min-h-[600px] flex-col max-[639px]:h-[100dvh] max-[639px]:min-h-0">
       <ErrorToast />
-      <Header wristHint={options.wristHint} />
       <div className="grid min-h-0 flex-1 grid-cols-[1fr_300px] overflow-hidden max-[1024px]:grid-cols-[1fr_240px] max-[639px]:flex max-[639px]:flex-col">
         <Studio />
         <Sidebar />
